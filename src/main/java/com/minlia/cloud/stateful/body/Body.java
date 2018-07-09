d4ed294f22c8bedaa13e72fb7257e7fd24145679
will <will@minlia.com>
2018-07-09 17:49:46 +0800

    Update

--- src/main/java/com/minlia/cloud/stateful/body/Body.java
+++ src/main/java/com/minlia/cloud/stateful/body/Body.java
@@ -16,11 +16,17 @@
 package com.minlia.cloud.stateful.body;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 
 /**
  * @author will
- */ //@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
+ */
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY, getterVisibility = JsonAutoDetect.Visibility.NONE, setterVisibility = JsonAutoDetect.Visibility.NONE, isGetterVisibility = JsonAutoDetect.Visibility.NONE, creatorVisibility = JsonAutoDetect.Visibility.NONE)
+@JsonIgnoreProperties(ignoreUnknown=true)
 public interface Body extends java.io.Serializable {
 
 }
--- src/main/java/com/minlia/cloud/stateful/body/WithIdItemBody.java
+++ src/main/java/com/minlia/cloud/stateful/body/WithIdItemBody.java
@@ -2,6 +2,7 @@ package com.minlia.cloud.stateful.body;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import java.io.Serializable;
 import java.util.List;
 import lombok.Data;
 
@@ -11,8 +12,8 @@ import lombok.Data;
  */
 @Data
 @ApiModel(value = "withIdItemBody",description = "带多个Id项的主体")
-public class WithIdItemBody implements Body {
+public class WithIdItemBody<ID extends Serializable> implements Body {
 
   @ApiModelProperty(value = "items",notes = "Items")
-  private List<WithIdBody> items;
+  private List<WithIdBody<ID>> items;
 }
